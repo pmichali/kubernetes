b337e72f689eb749b322007d9b041739dad7592e
Paul Michali <pc@michali.net>
2017-09-29 13:01:01 +0000

    Hack to leave conntrack max per core zero, so that later, kube-proxy doesn't try to update hashsize (if max connections > 4 * hashsize).
    Appears to be inconsistency between help info in CLI (which says max
    and max per core can be zero to leave alone), and default used (which
    sets max per core to 32K, if both max and max per core are zero).
    
    This wouldn'be be needed, but it appears that when running kube-proxy
    in a docker-in-docker environment, the sysfs file system says it allows
    read-write access, but we are unable to update hashsize, when writing
    to /sys/module/nf_conntrack/parameters/hashsize.
    
    There is an issue for the latter:
    https://github.com/kubernetes/kubernetes/issues/25543#issuecomment-282503251
    
    I'll create an issue for the former. Until then, we hack it out.

--- pkg/apis/componentconfig/v1alpha1/defaults.go
+++ pkg/apis/componentconfig/v1alpha1/defaults.go
@@ -79,9 +79,10 @@ func SetDefaults_KubeProxyConfiguration(obj *KubeProxyConfiguration) {
 	// If ConntrackMax is set, respect it.
 	if obj.Conntrack.Max == 0 {
 		// If ConntrackMax is *not* set, use per-core scaling.
-		if obj.Conntrack.MaxPerCore == 0 {
-			obj.Conntrack.MaxPerCore = 32 * 1024
-		}
+		// PCM
+		// if obj.Conntrack.MaxPerCore == 0 {
+		// 	obj.Conntrack.MaxPerCore = 32 * 1024
+		// }
 		if obj.Conntrack.Min == 0 {
 			obj.Conntrack.Min = 128 * 1024
 		}
